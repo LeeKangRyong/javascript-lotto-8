620b03bb2399f3d0c4021337436f9ecf047d6e4d
LeeKangRyong <gaiogo2@naver.com>
2025-10-31 17:59:44 +0900

    fix: Fix desc to asc sorting

--- src/views/output/utils/OutputFormatter.js
+++ src/views/output/utils/OutputFormatter.js
@@ -1,5 +1,13 @@
-// lotto format (오름차순으로 정렬하기)
+class OutputFormatter {
 
-// money format (, 넣어주기)
+    // lotto format (오름차순으로 정렬하기)
+    static sortLottobyAsc(lotto) {
+        return lotto.sort((a, b) => a - b);
+    }
 
-// 수익률 format (둘째 자리에서 반올림하기, model에서 계산은 정확히)
+    // money format (, 넣어주기)
+
+    // 수익률 format (둘째 자리에서 반올림하기, model에서 계산은 정확히)
+}
+
+export { OutputFormatter };
