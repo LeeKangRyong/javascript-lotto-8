e7eaa86e8ccb69e5a60e6f01437de28892610d12
LeeKangRyong <gaiogo2@naver.com>
2025-11-01 16:01:18 +0900

    refactor: Change way to test
    Change way to test accurately using checkErrorMessage function
    - Check error prefix
    - Check accurate error message

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,14 +1,22 @@
 import { Lotto } from '../src/models/index.js';
+import { checkErrorMessage } from '../src/shared/index.js';
+import { LOTTO_ERROR } from '../src/models/index.js';
 
 describe("로또 클래스 테스트", () => {
-    test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+    test("1. 로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
         expect(() => {
             new Lotto([1, 2, 3, 4, 5, 6, 7]);
-        }).toThrow("[ERROR]");
+        }).toThrow(checkErrorMessage(LOTTO_ERROR.SIX_NUMBERS));
+    });
+
+    test("2. 로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+        expect(() => {
+            new Lotto([1, 2, 3, 4, 5, 5]);
+        }).toThrow(checkErrorMessage(LOTTO_ERROR.DUPLICATED_NUMBER_EXISTS));
     });
 
     // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-    test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+    test("3. 로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
         expect(() => {
             new Lotto([1, 2, 3, 4, 5, 5]);
         }).toThrow("[ERROR]");
--- src/shared/index.js
+++ src/shared/index.js
@@ -2,3 +2,5 @@ export { WoowaError } from './error/WoowaError.js';
 export { ERROR_PREFIX } from './error/utils/ErrorConstants.js';
 
 export { NumberValidator } from './number/utils/NumberValidators.js';
+
+export { checkErrorMessage } from './test/checkErrorMessage.js';
